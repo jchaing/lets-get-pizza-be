4168393278a3e8ec1f5e43fb21dac8a9716a14ff
john <john@spinningplates.org>
2021-05-04 15:48:07 -0700

    server.js manually added cors

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //Authorization
 server.use("/api/auth/user", userauthRouter);
